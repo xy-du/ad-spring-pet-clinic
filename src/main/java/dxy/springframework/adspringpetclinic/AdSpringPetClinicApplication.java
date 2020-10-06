0c70977eca4cb7f5db7c273c0b0c07185eb4e64e
xyDu <xy_du@qq.com>
2020-10-06 15:25:56 +0800

    Initial Project Add

---
+++ src/main/java/dxy/springframework/adspringpetclinic/AdSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package dxy.springframework.adspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdSpringPetClinicApplication.class, args);
+    }
+
+}
