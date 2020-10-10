f179e44189f259a29662af465c7e70e40101c522
xyDu <xy_du@qq.com>
2020-10-10 19:52:29 +0800

    add index page and controller. close #8

---
+++ pet-clinic-web/src/main/java/dxy/springframework/adspringpetclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package dxy.springframework.adspringpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author AD
+ * @date 2020/10/10
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
