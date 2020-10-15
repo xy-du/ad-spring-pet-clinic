e46e12873c50fb9baef47f3c357d9c287525bb90
xyDu <xy_du@qq.com>
2020-10-15 20:14:58 +0800

    added Visit Entity and Updata Owner Entity with Pet. close #26

--- pet-clinic-data/src/main/java/dxy/springframework/adspringpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/dxy/springframework/adspringpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package dxy.springframework.adspringpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author AD
  * @date 2020/10/09
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
