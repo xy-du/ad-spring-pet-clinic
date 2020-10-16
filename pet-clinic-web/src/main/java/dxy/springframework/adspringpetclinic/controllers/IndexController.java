119b663f2e127806f30f8bad641cf533229a1367
xyDu <xy_du@qq.com>
2020-10-16 15:03:14 +0800

    show "not implemented" for broken links in index page. close #45

--- pet-clinic-web/src/main/java/dxy/springframework/adspringpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/dxy/springframework/adspringpetclinic/controllers/IndexController.java
@@ -15,4 +15,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String oups(){
+        return "notimplemented";
+    }
 }
