b9bec29761dfbc9d6ac2e7ef2b92c74c09104b40
xyDu <xy_du@qq.com>
2020-10-20 16:22:11 +0800

    JUnit4-to-JUnit5 update setup. close #4

--- pet-clinic-web/src/test/java/dxy/springframework/adspringpetclinic/AdSpringPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/dxy/springframework/adspringpetclinic/AdSpringPetClinicApplicationTest.java
@@ -1,16 +1,16 @@
 package dxy.springframework.adspringpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * @author AD
  * @date 2020/10/20
  */
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class AdSpringPetClinicApplicationTest {
     @Test
