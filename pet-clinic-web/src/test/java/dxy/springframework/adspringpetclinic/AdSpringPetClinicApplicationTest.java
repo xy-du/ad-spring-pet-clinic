70bbf2921771d68013aac46f7c708ba320d22b68
xyDu <xy_du@qq.com>
2020-10-20 14:00:40 +0800

    set Test to JUnit4 status.

---
+++ pet-clinic-web/src/test/java/dxy/springframework/adspringpetclinic/AdSpringPetClinicApplicationTest.java
@@ -0,0 +1,20 @@
+package dxy.springframework.adspringpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author AD
+ * @date 2020/10/20
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdSpringPetClinicApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+
+}
