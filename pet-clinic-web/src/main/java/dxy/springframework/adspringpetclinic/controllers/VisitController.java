ddab1d140b90e7e204aa2a41a4c0d4130027edb9
xyDu <xy_du@qq.com>
2020-10-27 20:57:16 +0800

    spring mvc test for visit controller. close #63

--- pet-clinic-web/src/main/java/dxy/springframework/adspringpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/dxy/springframework/adspringpetclinic/controllers/VisitController.java
@@ -9,6 +9,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 /**
@@ -26,8 +28,15 @@ public class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
