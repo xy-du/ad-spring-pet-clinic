0db9485b8b523e14e1aac10cc9be668a96bca0af
xyDu <xy_du@qq.com>
2020-10-28 10:57:11 +0800

    add json endpoint for vets list. close #65

--- pet-clinic-web/src/main/java/dxy/springframework/adspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dxy/springframework/adspringpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package dxy.springframework.adspringpetclinic.controllers;
 
+import dxy.springframework.adspringpetclinic.model.Vet;
 import dxy.springframework.adspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * @author AD
@@ -18,9 +22,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"vets", "vets/index", "vets/index.html","/vets.html"})
+    @RequestMapping({"vets", "vets/index", "vets/index.html", "/vets.html"})
     public String listVets(Model model) {
-        model.addAttribute("vets",vetService.findAll());
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
